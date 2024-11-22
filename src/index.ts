3c3a50869b81fb1fe0f0095b688e75d84108a97f
Yash Hegde <yash4success@gmail.com>
2024-11-22 17:41:56 +0530

    sending image to frontend

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Expresssss!');
+});
+
+app.post('/thumbnail', (req :any , res:any) => {
+  const userIdea = req.body.userIdea;
+  
+  if (!userIdea) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a search term in the "userIdea" field.',
+    });
+  }
+
+  // TODO: AI logic here: Respond with 3 image URLs
+  const images = [
+    `https://images.app.goo.gl/Cc3rAmbTEJe2L5gF9`,
+    `https://images.app.goo.gl/Cc3rAmbTEJe2L5gF9`,
+    `https://images.app.goo.gl/Cc3rAmbTEJe2L5gF9`
+  ];
+
+  res.json({
+    success: true,
+    searchTerm: userIdea,
+    images,
+  });
 });
 
 app.listen(PORT, () => {
